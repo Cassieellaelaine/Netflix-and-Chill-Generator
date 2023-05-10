dc9395b6df621df0347f893f9e859c312bc519f6
Mary <mcchristy@Marys-MacBook-Pro.local>
2023-05-10 18:31:53 -0400

    edited cocktailapi function to include function that will extract specif data from the object into a variable that can be pasted to the application

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,40 @@
 var specificGenreBtn = document.getElementsByClassName('dropdown-item');
 var randomCocktail = document.getElementById('randomcocktail');
 
-function getApi() {
+function getcocktailApi() {
     var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 
     fetch(requestUrl)
-        .then(function (repsonse) {
-            return repsonse.json();
+        .then(function (response) {
+            return response.json();
+        
         })
         .then(function (data) {
-            for (var i = 0; i < data.length; i++) {
-                console.log(data);
-                var cocktail = document.createElement('h4');
-                cocktail.textContent = data[i].text();
-                randomCocktail.appendChild(cocktail);
+            console.log(data.drinks[0])
+            var drinkName = data.drinks[0].strDrink
+            var drinkInstructions = data.drinks[0].strInstructions
+            var drinkIngredients = []
+            for (var i = 1; i < 16; i++) {
+                var value = data.drinks[0]["strIngredient"+i]
+                if (value === null ){break}
+                else { 
+                   drinkIngredients.push(value) 
+                }
+                //console.log(data);
+                // var cocktail = document.createElement('h4');
+                // cocktail.textContent = data[i].text();
+                // randomCocktail.appendChild(cocktail);
 
             }
+            console.log(drinkIngredients)
+
+            //'dropdown-item'.addEventListener("click",displayFunction);
+            //function displayFunction() {
+
+            //}
+
         })
 
 }
 
-getAPI();
+getcocktailApi();
