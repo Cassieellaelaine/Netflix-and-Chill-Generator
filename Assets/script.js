2311378ab9f21e8028fa6fedf6f2d95e85ece27d
Mary <mcchristy@Marys-MacBook-Pro.local>
2023-05-15 19:03:47 -0400

    created local storage file

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,13 @@
 var specificGenreBtn = document.getElementsByClassName('dropdown-item');
 var randomCocktail = document.getElementById('randomcocktail');
+var drinkName = document.createElement('h7');
+var drinkIngredients = document.createElement('p');
+var drinkInstructions = document.createElement('p');
 var comedyBtn = document.getElementById('comedyBtn');
 var dramaBtn = document.getElementById('dramaBtn');
 var sciFiBtn = document.getElementById('sciFiBtn');
 var romanceBtn = document.getElementById('romanceBtn');
+var newCocktail = document.getElementById('newcocktail');
 
 function getcocktailApi() {
     var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
@@ -15,9 +19,9 @@ function getcocktailApi() {
         })
         .then(function (data) {
             console.log(data.drinks[0])
-            var drinkName = data.drinks[0].strDrink
-            var drinkInstructions = data.drinks[0].strInstructions
-            var drinkIngredients = []
+            drinkName.innerHTML = data.drinks[0].strDrink
+            drinkInstructions.innerHTML = data.drinks[0].strInstructions
+            drinkIngredients.innerHTML = []
             for (var i = 1; i < 16; i++) {
                 var value = data.drinks[0]["strIngredient" + i]
                 if (value === null) { break }
@@ -30,9 +34,15 @@ function getcocktailApi() {
                 randomCocktail.firstChild.remove()
             }
             randomCocktail.append(drinkName, drinkIngredients, drinkInstructions);
-        })
-    //append or show drinkName drinkIngredients drinkInstructions
+            //append or show drinkName drinkIngredients drinkInstructions
+            //create function to display another random drink on button click
+            $('#newcocktail').click(function getcocktailApi () {
+    
+                randomCocktail.append(drinkName, drinkIngredients,drinkInstructions);
+            
+            })
 
+})
 }
 
 //getcocktailApi();
@@ -41,3 +51,21 @@ comedyBtn.addEventListener('click', getcocktailApi);
 dramaBtn.addEventListener('click', getcocktailApi);
 sciFiBtn.addEventListener('click', getcocktailApi);
 romanceBtn.addEventListener('click', getcocktailApi);
+
+
+
+//save show to local storage in watchlist
+$(document).ready(function () {
+
+    $('saveWatchlistBtn').on('click', function(){
+        var show = {title:'title', rating:'rating', synopsis: 'synopsis'};
+        var watchlist = $('.corner-button');
+        var showObj = JSON.stringify(show);
+        localStorage.setItem(showObj, watchlist);
+
+        
+    })
+
+    //view saved shows on watchlist
+    
+})
